a63be8e8648ad68d6283d6cee2c9a250a24ab722
vincent178 <vincent.007.cn@gmail.com>
2013-08-17 15:46:19 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
- 
+
+# Install vundle 
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+
+# copy all dotfiles
 for file in `find . | grep "\./[a-zA-Z]*/\."`; do
   echo ${file}
   cp ${file} ~/
 done
 
-echo -e "All files done! Enjoy!"
+echo "**************************************************"
+echo "*********  All files done! Enjoy *****************"
+echo "**************************************************"
