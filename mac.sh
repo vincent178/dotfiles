631b3aa499b26f360b3e80d7a7b4a23212f66617
vincent178 <vh7157@gmail.com>
2020-01-20 13:39:03 +0800

    update mac script

--- mac.sh
+++ mac.sh
@@ -4,24 +4,42 @@ set -e
 
 echo "Setup your Mac..."
 
-if test ! $(which brew); then
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-fi
+check_or_install () {
+	if ! [ -x "$(command -v $1)" ]; then
+		echo "not found $1, try to install it"
+		if [ "$1" == "brew" ]; then
+			/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+		else if [ "$1" == "zsh" ]; then
+			sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+		else
+			brew install $1
+		fi
+	fi
+}
 
+echo "install dotfiles"
+
+echo "setup homebrew"
+check_or_install "homebrew"
 brew update
 brew tap homebrew/bundle
 brew bundle
 
+echo "setup zshrc"
+# ln -s $PWD/.zshrc $HOME/.zshrc
+
+echo "setup slate"
+# ln -s $HOME/Dotfiles/.slate $HOME/.slate
+
+echo "setup oh-my-zsh"
 if [ ! -d $HOME/.oh-my-zsh ]; then
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 	rm -rf $HOME/.zshrc
 fi
 
-ln -s $HOME/Dotfiles/.zshrc $HOME/.zshrc
-ln -s $HOME/Dotfiles/.slate $HOME/.slate
-
+echo "setup git"
 # config git ignore
-ln -s $HOME/Dotfiles/.gitignore $HOME/.gitignore
+ln -s $PWD/.gitignore $HOME/.gitignore
 git config --global core.excludesfile ~/.gitignore
 
 source .macos
