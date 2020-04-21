41013ab2a2c2d2c16ce9e0e0b85c5ef06f5b4301
vincent178 <vh7157@gmail.com>
2020-04-21 20:36:10 +0800

    add .p10k.zsh file

--- linux.sh
+++ linux.sh
@@ -42,6 +42,8 @@ echo "setup git config"
 echo "setup zsh"
 [ ! -f $HOME/.zshrc ] && \
     ln -s $PWD/.zshrc $HOME/.zshrc
+[ ! -f $HOME/.p10k.zsh ] && \
+    ln -s $PWD/.p10k.zsh $HOME/.p10k.zsh
 
 [ ! -f $HOME/.ripgreprc ] && \
     ln -s $PWD/.ripgreprc $HOME/.ripgreprc
