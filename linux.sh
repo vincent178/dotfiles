cb7a507acf5cd8058b6aeffaae383f3fb20427d1
vincent178 <vh7157@gmail.com>
2020-04-30 10:38:21 +0800

    continue tuning alacritty tmux and bash

--- linux.sh
+++ linux.sh
@@ -2,6 +2,8 @@
 
 set -ueo pipefail
 
+# https://arslan.io/2019/07/03/how-to-write-idempotent-bash-scripts/
+
 check_or_install () {
 	if ! [ -x "$(command -v $1)" ]; then
 		echo "not found $1, try to install it"
@@ -14,12 +16,12 @@ echo "install dotfiles"
 echo "setup nvim"
 check_or_install "nvim"
 
-[ ! -d $HOME/.config/nvim/ ] && \
-    mkdir $HOME/.config/nvim/ && \
-    mkdir $HOME/.config/coc/ && \
-    ln -s $PWD/init.vim $HOME/.config/nvim/init.vim && \
-    ln -s $PWD/coc-settings.json $HOME/.config/nvim/coc-settings.json && \
-    ln -s $PWD/coc $HOME/.config/coc
+mkdir -p $HOME/.config/nvim/ \
+    mkdir -p $HOME/.config/coc/ \
+    ln -fsn $PWD/init.vim $HOME/.config/nvim/init.vim \
+    ln -fsn $PWD/coc-settings.json $HOME/.config/nvim/coc-settings.json \
+    ln -fsn $PWD/coc $HOME/.config/coc
+
 
 [ ! -f $HOME/.local/share/nvim/site/autoload/plug.vim ] && \
     curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
@@ -27,26 +29,21 @@ check_or_install "nvim"
 
 echo "setup tmux"
 check_or_install "tmux"
-[ ! -f $HOME/.tmux.conf ] && \
-	ln -s $PWD/.tmux.conf $HOME/.tmux.conf
+ln -fsn $PWD/.tmux.conf $HOME/.tmux.conf
 [ ! -d $HOME/.tmux/plugins ] && \
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 
 echo "setup git config"
-[ ! -f $HOME/.gitignore ] && \
-	ln -s $PWD/.gitignore $HOME/.gitignore
-[ ! -f $HOME/.gitconfig ] && \
-	ln -s $PWD/.gitconfig $HOME/.gitconfig
+ln -fsn $PWD/.gitignore $HOME/.gitignore
+ln -fsn $PWD/.gitconfig $HOME/.gitconfig
 
 echo "setup zsh"
 [ ! -f $HOME/.zshrc ] && \
     ln -s $PWD/.zshrc $HOME/.zshrc
-[ ! -f $HOME/.p10k.zsh ] && \
-    ln -s $PWD/.p10k.zsh $HOME/.p10k.zsh
 
-[ ! -f $HOME/.ripgreprc ] && \
-    ln -s $PWD/.ripgreprc $HOME/.ripgreprc
+ln -fsn $PWD/.p10k.zsh $HOME/.p10k.zsh
+ln -fsn $PWD/.ripgreprc $HOME/.ripgreprc
 
 echo "setup nvm"
 [ ! -d $HOME/.nvm ] && \
