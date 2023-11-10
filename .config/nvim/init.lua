ef1ada6f858047d6a248f61c2adc74dd08870027
vincent178 <vh7157@gmail.com>
2023-11-11 07:06:21 +0800

    fix: add fzf build

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -241,7 +241,10 @@ require("lazy").setup({
     },
 
     -- Fuzzy Finder
-    'nvim-telescope/telescope-fzf-native.nvim',
+    { 
+        'nvim-telescope/telescope-fzf-native.nvim',
+        build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
+    },
     'nvim-telescope/telescope-live-grep-args.nvim',
     {
         'nvim-telescope/telescope.nvim',
