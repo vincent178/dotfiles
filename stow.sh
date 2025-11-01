29794a4ae02b0e0279ff54c7a8cc3262a9debfd5
vincent178 <vh7157@gmail.com>
2025-11-01 18:39:42 +0800

    ssh config

--- stow.sh
+++ stow.sh
@@ -4,6 +4,10 @@ stow zsh -t $HOME
 stow tmux -t $HOME
 stow scripts -t $HOME
 
+# setup ssh config
+mkdir -p .ssh
+stow --target=$HOME/.ssh ssh
+
 mkdir -p $HOME/.config
 
 stow config -t $HOME/.config
